f7784a1de4b52ff4773af3057caa9f23984b5835
VijayVardhan-Dev <216757726+VijayVardhan-Dev@users.noreply.github.com>
2026-01-26 14:34:24 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ agronet/Frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1 className='text-4xl text-red-500 font-bold underline'>AgroNet</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
